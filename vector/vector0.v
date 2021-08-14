3492a17b9bece1177ca1cbda96c1448f63dee33d
deepanjalimishra99 <88837729+deepanjalimishra99@users.noreply.github.com>
2021-08-14 17:39:45 +0530

    Create vector0.v

---
+++ vector/vector0.v
@@ -0,0 +1,13 @@
+module top_module(
+  input wire [2:0] vec,
+  output wire [2:0] outv,
+  output wireo0,
+  output wire o1,
+  output wire o2);
+  
+  assign outv = vec;
+  assign o0 = vec[0];
+  assign o1 = vec[1];
+  assign o2 = vec[2];
+  
+  endmodule
