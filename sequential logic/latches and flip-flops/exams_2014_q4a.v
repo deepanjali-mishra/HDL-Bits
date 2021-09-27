738c6b0f7135ebb0f32e49c0f8ae55f9b4ddd828
deepanjalimishra99 <88837729+deepanjalimishra99@users.noreply.github.com>
2021-09-27 20:55:53 +0530

    Create exams_2014_q4a.v

---
+++ sequential logic/latches and flip-flops/exams_2014_q4a.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output Q
+);
+    wire w1,w2;
+    always @(posedge clk) begin
+        Q <= w2;
+    end
+    
+    assign w1 = (E) ? w : Q;
+    assign w2 = (L) ? R : w1;
+
+endmodule
