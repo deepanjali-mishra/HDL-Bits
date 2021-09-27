02514c3422d8e24b2a9800cb69fd7f44092887d7
deepanjalimishra99 <88837729+deepanjalimishra99@users.noreply.github.com>
2021-09-27 20:51:39 +0530

    Create mt2015_muxdff.v

---
+++ sequential logic/latches and flip-flops/mt2015_muxdff.v
@@ -0,0 +1,15 @@
+module top_module (
+	input clk,
+	input L,
+	input r_in,
+	input q_in,
+	output reg Q);
+    wire w1;
+    
+    always @(posedge clk) begin
+        Q <= w1;
+    end
+    
+    assign w1 = (L) ? r_in : q_in;
+    
+endmodule
